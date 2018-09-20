4b34a6b6a957ca0ac047b7bcce60052096a6dae8
robin <webtjw@foxmail.com>
2018-09-20 08:43:30 +0800

    限定编辑器最大高度

--- src/components/mobileEditor/MobileEditor.vue
+++ src/components/mobileEditor/MobileEditor.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="mobile-editor">
     <div class="mock-mobile" ref="mockMobile" :style="{
-      width: `${mobileType.width}px`,
-      height: `${mobileType.height}px`,
+      width: `${editorSize.width}px`,
+      height: `${editorSize.height}px`,
     }">
     </div>
   </div>
 </template>
 
 <script>
+const docHeight = document.documentElement.clientHeight
+const maxScale = 0.9
+
 export default {
   props: {
     mobileType: { required: true, type: Object }
   },
+  data () {
+    return {
+      maxHeight: (docHeight - 68) * maxScale
+    }
+  },
+  computed: {
+    editorSize () {
+      const { maxHeight, mobileType, mobileType: { width, height } } = this
+      return height <= maxHeight
+        ? mobileType
+        : { width: parseInt(maxHeight / height * width, 10), height: maxHeight }
+    }
+  },
   mounted () {
     const rect = this.$refs.mockMobile.getBoundingClientRect()
-    const maxHeight = parseInt(document.documentElement.clientHeight - rect.top, 10)
-    console.log(maxHeight) // 根据该值设置理想高度
+    console.log(this.maxHeight = parseInt((docHeight - rect.top) * maxScale, 10))
   }
 }
 </script>
